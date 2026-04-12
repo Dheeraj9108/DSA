786da18966a712e0f26c1a056db370c13e46b290
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-04-12 12:10:31 +0530

    Time: 13 ms (63.08%) | Memory: 47.1 MB (56.92%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,33 +1,39 @@
 class Solution {
-    private int solve(String word, int f1x, int f1y, int f2x, int f2y, int idx,int dp[][][][][]){
-        if(idx >= word.length()) return 0;
+    private int solve(String word, int f1, int f2, int idx, int dp[][][]) {
+        if (idx >= word.length())
+            return 0;
 
-        if(dp[f1x+1][f1y+1][f2x+1][f2y+1][idx] != -1) return dp[f1x+1][f1y+1][f2x+1][f2y+1][idx];
+        if (dp[f1+1][f2+1][idx] != -1)
+            return dp[f1+1][f2+1][idx];
         int ch = word.charAt(idx) - 'A';
-        int x = ch/6;
-        int y = ch%6;
+        int x = ch / 6;
+        int y = ch % 6;
         int distf1 = 0;
         int distf2 = 0;
-        if(f1x != -1 && f1y != -1) distf1 = Math.abs(x - f1x) + Math.abs(y - f1y);
-        if(f2x != -1 && f2y != -1) distf2 = Math.abs(x - f2x) + Math.abs(y - f2y);
+        if (f1 != -1) {
+            int f1x = f1 / 6;
+            int f1y = f1 % 6;
+            distf1 = Math.abs(x - f1x) + Math.abs(y - f1y);
+        }
+        if (f2 != -1) {
+            int f2x = f2 / 6;
+            int f2y = f2 % 6;
+            distf2 = Math.abs(x - f2x) + Math.abs(y - f2y);
+        }
 
-        int nextf1 = distf1 + solve(word,x,y,f2x,f2y,idx+1,dp);
-        int nextf2 = distf2 + solve(word,f1x,f1y,x,y,idx+1,dp);
-        return dp[f1x+1][f1y+1][f2x+1][f2y+1][idx] = Math.min(nextf1, nextf2);
+        int nextf1 = distf1 + solve(word, ch, f2, idx + 1, dp);
+        int nextf2 = distf2 + solve(word, f1, ch, idx + 1, dp);
+        return dp[f1+1][f2+1][idx] = Math.min(nextf1, nextf2);
     }
+
     public int minimumDistance(String word) {
         int n = word.length();
-        int dp[][][][][] = new int[7][8][7][8][n+1];
-        for(int a[][][][] : dp){
-            for(int b[][][] : a){
-                for(int c[][] : b){
-                    for(int d[] : c) Arrays.fill(d,-1);
-                }
+        int dp[][][] = new int[27][27][n + 1];
+        for (int a[][] : dp) {
+            for (int b[] : a) {
+                Arrays.fill(b, -1);
             }
         }
-        int ch = word.charAt(0) - 'A';
-        int x = ch/6;
-        int y = ch%6;
-        return solve(word,-1,-1,-1,-1,0,dp);
+        return solve(word, -1, -1, 0, dp);
     }
 }
