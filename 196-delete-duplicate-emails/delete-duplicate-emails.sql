8a7e3261d08997f6ea59d0ce3d93663bbe1cf4cc
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-04-25 20:32:13 +0530

    Time: 570 ms (62.81%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,8 @@
 /* Write your PL/SQL query statement below */
-delete from person where id in ( 
-    select id from ( 
-        select id, row_number() over(partition by email order by id) r from person
-    ) where r > 1
-)
+-- delete from person where id in ( 
+--     select id from ( 
+--         select id, row_number() over(partition by email order by id) r from person
+--     ) where r > 1
+-- )
+
+delete from Person where id not in (select min(id) from Person group by email)
