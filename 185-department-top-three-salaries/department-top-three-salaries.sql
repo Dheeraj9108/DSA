cb7f2049735415d514738d6015052d19c9d50ad1
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-04-25 18:57:43 +0530

    Time: 804 ms (72.18%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,7 +1,15 @@
 /* Write your PL/SQL query statement below */
 
--- select salary, dense_rank() over ()
-
--- select distinct e.salary,e.departmentId,dense_rank() over(partition by departmentId order by salary) rank from employee e order by e.departmentId;
-
-select Department, Employee, Salary from (select d.name as Department, e.name as Employee,e.salary, dense_rank() over(partition by e.departmentId order by e.salary desc) rank from Employee e JOIN Department d on e.departmentId = d.id) where rank <= 3
+select 
+    Department, 
+    Employee, 
+    Salary 
+    from (
+        select 
+            d.name as Department, 
+            e.name as Employee,
+            e.salary,
+            dense_rank() over(partition by e.departmentId order by e.salary desc) rank 
+        from Employee e JOIN Department d on e.departmentId = d.id
+    ) 
+where rank <= 3
