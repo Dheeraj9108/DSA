f421bbed24b9ea83e413e081d12cb92f71084415
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-05-16 22:45:20 +0530

    Time: 1180 ms (73.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+
+select name from SalesPerson where sales_id not in (select sales_id from Orders o LEFT JOIN Company c on c.com_id = o.com_id where c.name = 'RED')
+-- where s.name = 'Amy'
