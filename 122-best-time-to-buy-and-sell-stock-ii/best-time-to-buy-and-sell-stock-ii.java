6e3d5fd8e432525552ec3e0026d4069d0bfa8ce3
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-06-28 14:29:45 +0530

    Time: 2 ms (16.99%) | Memory: 46.7 MB (17.61%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,29 +1,44 @@
 class Solution {
-    private int solve(int[] prices, int day, int buy, int dp[][]){
+    private int solve(int[] prices, int day, int buy, int dp[][]) {
 
-        if(day >= prices.length){
+        if (day >= prices.length) {
             return 0;
         }
 
-        if(dp[day][buy] != -1) return dp[day][buy];
+        if (dp[day][buy] != -1)
+            return dp[day][buy];
 
         int take = 0;
         int skip = 0;
-        if(buy == 1){
-            take =  solve(prices,day+1, 0, dp) - prices[day]; 
+        if (buy == 1) {
+            take = solve(prices, day + 1, 0, dp) - prices[day];
         } else {
-            take = prices[day] + solve(prices, day+1, 1, dp);
+            take = prices[day] + solve(prices, day + 1, 1, dp);
         }
 
-        skip = solve(prices, day+1, buy, dp);
+        skip = solve(prices, day + 1, buy, dp);
 
-        return dp[day][buy] = Math.max(take,skip);
+        return dp[day][buy] = Math.max(take, skip);
 
     }
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n][2];
-        for(int states[]: dp) Arrays.fill(states,-1);
-        return solve(prices, 0, 1, dp);
+        int dp[][] = new int[n+1][2];
+        // for(int states[]: dp) Arrays.fill(states,-1);
+        for (int day = prices.length - 1; day >= 0; day--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int take = 0;
+                int skip = 0;
+                if (buy == 1) {
+                    take = dp[day+1][0] - prices[day];
+                } else {
+                    take = prices[day] + dp[day+1][1];
+                }
+                skip = dp[day+1][buy];
+                dp[day][buy] = Math.max(take,skip);
+            }
+        }
+        return dp[0][1];
     }
 }
