8aeafc0e09b6ca1ea3dcaf0ebef00f0f2ee16657
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-08-01 12:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,18 +1,22 @@
 class Solution {
-    private int solve(int nums[], int i, int j){
+    private int solve(int nums[], int i, int j, int dp[][]){
         if(i > j) {
             return 0;
         }
 
-        int take_i = nums[i]+ Math.min(solve(nums,i+2,j),solve(nums,i+1,j-1));
-        int take_j = nums[j]+ Math.min(solve(nums,i,j-2),solve(nums,i+1,j-1));
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return Math.max(take_i, take_j);
+        int take_i = nums[i]+ Math.min(solve(nums,i+2,j,dp),solve(nums,i+1,j-1,dp));
+        int take_j = nums[j]+ Math.min(solve(nums,i,j-2,dp),solve(nums,i+1,j-1,dp));
+
+        return dp[i][j] = Math.max(take_i, take_j);
     }
     public boolean predictTheWinner(int[] nums) {
+        int dp[][] = new int[nums.length][nums.length];
+        for(int a[]:dp) Arrays.fill(a,-1);
         int total = 0;
         for(int ele : nums) total+=ele; 
-        int scorep1 = solve(nums, 0, nums.length-1);
+        int scorep1 = solve(nums, 0, nums.length-1, dp);
         int scorep2 = total - scorep1;
         return scorep1 >= scorep2; 
     }
