b7fce49662db7fadbda0eabedcea92f809e6f8ff
Dheeraj9108 <88208351+Dheeraj9108@users.noreply.github.com>
2026-09-06 10:31:32 +0530

    Time: 20 ms (53.25%) | Memory: 54.3 MB (62.97%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,21 +1,44 @@
 class Solution {
     int dp[][];
-    private int solve(int i, int j, String s, String t){
-        if(j >= t.length()) return 1;
 
-        if(i >= s.length()) return 0;
+    private int solve(int i, int j, String s, String t) {
+        if (j >= t.length())
+            return 1;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (i >= s.length())
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         int take = 0;
-        if(s.charAt(i) == t.charAt(j)) take = solve(i+1,j+1,s,t); 
-        int skip = solve(i+1,j,s,t);
-        return  dp[i][j] = take+skip;
+        if (s.charAt(i) == t.charAt(j))
+            take = solve(i + 1, j + 1, s, t);
+        int skip = solve(i + 1, j, s, t);
+        return dp[i][j] = take + skip;
     }
+
     public int numDistinct(String s, String t) {
-        dp = new int[s.length()+1][t.length()+1];
-        for(int row[] : dp) Arrays.fill(row,-1);
-        return solve(0,0,s,t);
+        int m = s.length();
+        int n = t.length();
+        dp = new int[m + 1][n + 1];
+        // for(int row[] : dp) Arrays.fill(row,-1);
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int take = 0;
+                if (s.charAt(i) == t.charAt(j))
+                    take = dp[i+1][j+1];
+                int skip = dp[i+1][j];
+                dp[i][j] = take + skip;
+            }
+        }
+        return dp[0][0];
+        // return solve(0, 0, s, t);
     }
 }
 
@@ -27,6 +50,5 @@ class Solution {
 // b        
 // i
 // t
-
 
 // r a b b b i t     r a b b i t
